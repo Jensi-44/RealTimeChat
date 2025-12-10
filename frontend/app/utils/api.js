dd8b0ecd3568c82dd21c1a30d045b88283142409
Jensi Kachhadiya <jensikachhadiya4@gmail.com>
2025-12-10 14:38:10 +0530

    url

--- frontend/app/utils/api.js
+++ frontend/app/utils/api.js
@@ -1,8 +1,8 @@
-export const backendUrl = "http://localhost:3001";
+// export const backendUrl = "http://localhost:3001";
 
 export async function fetchUsers() {
   const res = await fetch(`${backendUrl}/auth/users`);
   return res.json();
 }
 
-// export const backendUrl = " https://realtimechat-4uzm.onrender.com";
+export const backendUrl = " https://realtimechat-4uzm.onrender.com";
