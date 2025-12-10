9643d903594c9060375aa7da495202b90dbd4421
Jensi Kachhadiya <jensikachhadiya4@gmail.com>
2025-12-10 14:52:50 +0530

    url one add

--- frontend/app/utils/socket.ts
+++ frontend/app/utils/socket.ts
@@ -7,7 +7,7 @@
 // utils/socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+export const socket = io("https://realtimechat-4uzm.onrender.com", {
   // Change to your NestJS port
   withCredentials: true,
   transports: ["websocket"],
